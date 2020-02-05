56b0354ab3b12d2a4e9198bc2760fd5da07ef3f6
Jarvis <598494198@qq.com>
2020-02-06 00:13:37 +0800

    left panel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import FileSearch from './components/FileSearch.js'
+import FileList from './components/FileList.js'
+import defaultFile from './utils/defaultFile.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col-6 bg-light left-panel">
+          <FileSearch 
+            title="我的云笔记"
+            onFileSearch={(value) => {console.log(value)}}
+          />
+          <FileList 
+            files={defaultFile}
+          />
+        </div>
+        <div className="col-6 bg-primary right-panel">
+          <h1>this is right</h1>
+        </div> 
+      </div>
     </div>
   );
 }

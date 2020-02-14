699fd360e63a2ae37bcfaef8229cd48a211846e2
Jarvis <598494198@qq.com>
2020-02-14 20:21:56 +0800

    qiniu base finish

--- src/utils/qiniuManager.js
+++ src/utils/qiniuManager.js
@@ -1,5 +1,6 @@
 const qiniu = require('qiniu')
-
+const axios = require('axios')
+const fs = require('fs')
 class QiniuManager {
   constructor(accessKey, secretKey, bucket) {
     this.mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
@@ -37,6 +38,46 @@ class QiniuManager {
     const digest = qiniu.util.generateAccessToken(this.mac, reqURL)
     return new Promise((resolve, reject) => {
       qiniu.rpc.postWithoutForm(reqURL, digest, this._handleCallback(resolve, reject))
+    })
+  }
+  generatedDownloadLink(key) {
+    const domainPromise = this.publicBucketDomain ?
+      Promise.resolve([this.publicBucketDomain]) : this.getBucketDomain()
+    return domainPromise.then(data => {
+      if (Array.isArray(data) && data.length > 0) {
+        const pattern = /^http?/
+        this.publicBucketDomain = pattern.test(data[0]) ? data[0] : `http://${data[0]}`
+        return this.bucketManager.publicDownloadUrl(this.publicBucketDomain, key)
+      } else {
+        throw Error('域名未找到，请检检查存储空间是否过期')
+      }
+    })
+  }
+
+  downloadFile(key, downloadPath) {
+    //1. 获取 下载链接
+    //2. 向这个下载链接发送请求，返回一个可读流
+    //3. 创建一个可写流，可读流到可写流的转换(容器)
+    //4. 将结果构造成一个promise对象
+    return this.generatedDownloadLink(key).then((link) => {
+      const timeStamp = new Date().getTime()
+      const url = `${link}?timestamp=${timeStamp}`
+      return axios({
+        url,
+        method: 'GET',
+        responseType: 'stream',
+        headers: {'Cache-Control': 'no-cache'}
+      }).then(response => {
+        const writer = fs.createWriteStream(downloadPath)
+        response.data.pipe(writer)
+        return new Promise((resolve, reject) => {
+          writer.on('finish', resolve)
+          writer.on('error', reject)
+        })
+      }).catch(err => {
+        return Promise.reject({err: err.response})
+      })
+      
     })
   }
   _handleCallback(resolve, reject) {
